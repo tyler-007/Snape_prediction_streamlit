624af9a2e463be6b9e5a41040bb7c376407a4bd7
tyler-007 <123353445+tyler-007@users.noreply.github.com>
2024-06-13 13:13:11 +0530

    Update master_script.py

--- master_script.py
+++ master_script.py
@@ -6,31 +6,23 @@ import laggeddata
 import weatherunion_script
 import model_predict
 
-print("Script_fetch_from_db ...")
 Script_fetch_from_db.run()
 
 # Execute script2
-print("Parse script...")
 Parse.run()
 
-print("process script...")
 process.run()
 
 # Execute script2
-print("dataprocess script...")
 dataprocess.run()
 
-print("laggeddata script...")
 laggeddata.run()
 
 # Execute script2
-print("weatherunion_script script...")
 weatherunion_script.run()
 
-print("model_predict script...")
 model_predict.run()
 
-print("model_predict script...")
 app.run()
 
 
